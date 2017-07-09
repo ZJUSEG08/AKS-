65b33ab0bf19d4449ed342d626042a6e284cd63a
wy-fu <wy.fu@foxmail.com>
2017-07-09 14:36:25 +0800

    OrderStructure+DeviceStructure

---
+++ app/src/main/java/com/example/isacclee/firsthello/OrderStructure.java
@@ -0,0 +1,25 @@
+package com.example.isacclee.firsthello;
+
+/**
+ * Created by Pro15 on 17/7/9.
+ */
+
+public class OrderStructure {
+    public class States{
+        public String time;
+        public String state;
+    }
+
+    public String orderID;
+    public String goodsID;
+    public int number;
+    public String state;
+    public String creatingTime;
+    public String sendingTime;
+    public String receiver;
+    public String address;
+    public String supplier;
+    public String expressID;
+    public States statesList;
+
+}
