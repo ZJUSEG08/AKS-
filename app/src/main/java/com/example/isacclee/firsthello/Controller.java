7239fc3ddf6ae05dec6a1c7072f3dacc90befeb6
wy-fu <wy.fu@foxmail.com>
2017-07-09 14:28:33 +0800

    Controller

---
+++ app/src/main/java/com/example/isacclee/firsthello/Controller.java
@@ -0,0 +1,275 @@
+package com.example.isacclee.firsthello;
+
+import org.json.*;
+
+
+
+/**
+ * Created by Pro15 on 17/7/9.
+ */
+
+public class Controller {
+
+    public int SignUp(String email,String password,String phone) {
+
+        String ToServerString=null;
+        JSONObject ToServer = new JSONObject();
+        int answer=0;
+        try {
+            ToServer.put("email", email);
+            ToServer.put("password", password);
+            ToServer.put("phone", phone);
+            ToServerString = ToServer.toString();
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        try{
+            Connection HeartBeatConnection=new Connection("SignUp");
+            HeartBeatConnection.send(ToServerString);
+            String line;
+            while ((line = HeartBeatConnection.br.readLine()) != null) {
+                //接受请求结果
+                JSONObject fromServer=new JSONObject(line);
+                String result=fromServer.getString("result");
+                if (result.equals("Y")){
+                    answer=1;
+                }else{
+                    answer=0;
+                }
+            }
+            HeartBeatConnection.drop();
+            //断开连接*/
+        }catch(Exception e){
+
+        };
+        return answer;
+    }
+
+    public int ForgetPassword(String email) {
+
+        String ToServerString=null;
+        JSONObject ToServer = new JSONObject();
+        int answer=0;
+        try {
+            ToServer.put("email", email);
+            ToServerString = ToServer.toString();
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        try{
+            Connection HeartBeatConnection=new Connection("ForgetPassword");
+            HeartBeatConnection.send(ToServerString);
+            String line;
+            while ((line = HeartBeatConnection.br.readLine()) != null) {
+                //接受请求结果
+                JSONObject fromServer=new JSONObject(line);
+                String result=fromServer.getString("result");
+                if (result.equals("Y")){
+                    answer=1;
+                }else{
+                    answer=0;
+                }
+            }
+            HeartBeatConnection.drop();
+            //断开连接*/
+        }catch(Exception e){
+
+        };
+        return answer;
+    }
+
+    public int SignIn(String email,String password) {
+
+        String ToServerString=null;
+        JSONObject ToServer = new JSONObject();
+        int answer=0;
+        try {
+            ToServer.put("email", email);
+            ToServer.put("password", password);
+            ToServerString = ToServer.toString();
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        try{
+            Connection HeartBeatConnection=new Connection("SignIn");
+            HeartBeatConnection.send(ToServerString);
+            String line;
+            while ((line = HeartBeatConnection.br.readLine()) != null) {
+                //接受请求结果
+                JSONObject fromServer=new JSONObject(line);
+                String result=fromServer.getString("result");
+                if (result.equals("Y")){
+                    answer=1;
+                }else{
+                    answer=0;
+                }
+            }
+            HeartBeatConnection.drop();
+            //断开连接*/
+        }catch(Exception e){
+
+        };
+        return answer;
+    }
+
+    public int EditInfo(String email,String password,String phone) {
+
+        String ToServerString=null;
+        JSONObject ToServer = new JSONObject();
+        int answer=0;
+        try {
+            ToServer.put("email", email);
+            ToServer.put("password", password);
+            ToServer.put("phone", phone);
+            ToServerString = ToServer.toString();
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        try{
+            Connection HeartBeatConnection=new Connection("EditInfo");
+            HeartBeatConnection.send(ToServerString);
+            String line;
+            while ((line = HeartBeatConnection.br.readLine()) != null) {
+                //接受请求结果
+                JSONObject fromServer=new JSONObject(line);
+                String result=fromServer.getString("result");
+                if (result.equals("Y")){
+                    answer=1;
+                }else{
+                    answer=0;
+                }
+            }
+            HeartBeatConnection.drop();
+            //断开连接*/
+        }catch(Exception e){
+
+        };
+        return answer;
+    }
+
+    public int CheckDevice(String email,String aksID) {
+
+        String ToServerString=null;
+        JSONObject ToServer = new JSONObject();
+        int answer=0;
+        try {
+            ToServer.put("email", email);
+            ToServer.put("aksID", aksID);
+            ToServerString = ToServer.toString();
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        try{
+            Connection HeartBeatConnection=new Connection("CheckDevice");
+            HeartBeatConnection.send(ToServerString);
+            String line;
+            while ((line = HeartBeatConnection.br.readLine()) != null) {
+                //接受请求结果
+                JSONObject fromServer=new JSONObject(line);
+                String state=fromServer.getString("state");
+                if (state.equals("New")){
+                    answer=-1;
+                }else{
+                    if (state.equals("Yes")){
+                        answer=1;
+                    }else{
+                        answer=0;
+                    }
+                }
+            }
+            HeartBeatConnection.drop();
+            //断开连接*/
+        }catch(Exception e){
+
+        };
+        return answer;
+    }
+
+    public void GetDevice(DeviceStructure Device) {
+
+        String ToServerString=null;
+        JSONObject ToServer = new JSONObject();
+        try {
+            ToServer.put("aksID", Device.aksID);
+            ToServerString = ToServer.toString();
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        try{
+            Connection HeartBeatConnection=new Connection("GetDevice");
+            HeartBeatConnection.send(ToServerString);
+            String line;
+            while ((line = HeartBeatConnection.br.readLine()) != null) {
+                //接受请求结果
+                JSONObject fromServer=new JSONObject(line);
+                Device.goodsID=fromServer.getString("goodsID");
+                Device.number=fromServer.getInt("number");
+                Device.receiverName=fromServer.getString("receiverName");
+                Device.address=fromServer.getString("address");
+                Device.postCode=fromServer.getString("postCode");
+                Device.phone=fromServer.getString("phone");
+                Device.orderLimit=fromServer.getInt("orderLimit");
+            }
+            HeartBeatConnection.drop();
+            //断开连接*/
+        }catch(Exception e){
+
+        };
+    }
+
+    public int UpdateDevice(DeviceStructure Device) {
+
+        String ToServerString=null;
+        JSONObject ToServer = new JSONObject();
+        int answer=0;
+        try {
+            ToServer.put("aksID", Device.aksID);
+            ToServer.put("goodsID", Device.goodsID);
+            ToServer.put("number", Device.number);
+            ToServer.put("receiverName", Device.receiverName);
+            ToServer.put("address", Device.address);
+            ToServer.put("postCode", Device.postCode);
+            ToServer.put("phone", Device.phone);
+            ToServer.put("orderLimit", Device.orderLimit);
+
+            ToServerString = ToServer.toString();
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        try{
+            Connection HeartBeatConnection=new Connection("UpdateDevice");
+            HeartBeatConnection.send(ToServerString);
+            String line;
+            while ((line = HeartBeatConnection.br.readLine()) != null) {
+                //接受请求结果
+                JSONObject fromServer=new JSONObject(line);
+                String result=fromServer.getString("result");
+                if (result.equals("Y")){
+                    answer=1;
+                }else{
+                    answer=0;
+                }
+            }
+            HeartBeatConnection.drop();
+            //断开连接*/
+        }catch(Exception e){
+
+        };
+        return answer;
+    }
+
+}
---
+++ app/src/main/java/com/example/isacclee/firsthello/DeviceStructure.java
@@ -0,0 +1,16 @@
+package com.example.isacclee.firsthello;
+
+/**
+ * Created by Pro15 on 17/7/9.
+ */
+
+public class DeviceStructure {
+    public String aksID;
+    public String goodsID;
+    public int number;
+    public String receiverName;
+    public String address;
+    public String postCode;
+    public String phone;
+    public int orderLimit;
+}
