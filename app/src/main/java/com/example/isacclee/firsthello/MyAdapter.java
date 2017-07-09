4d86d3fc09168782835dc8c1b9492ccbd19f5c93
Isacclee <447473281@qq.com>
2017-07-09 16:03:08 +0800

    Half of the Order list infomation finished

---
+++ app/src/main/java/com/example/isacclee/firsthello/MyAdapter.java
@@ -0,0 +1,55 @@
+package com.example.isacclee.firsthello;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Isacclee on 7/9/17.
+ */
+
+public class MyAdapter extends BaseAdapter{
+    private List<Data> mData;
+
+    private Context mContext;
+
+    public MyAdapter(List<Data> mData, Context mContext) {
+        this.mData = mData;
+        this.mContext = mContext;
+    }
+    @Override
+    public int getCount() {
+        return mData.size();
+    }
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+        if(convertView == null){
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.order_item,parent,false);
+            viewHolder = new ViewHolder();
+            viewHolder.order_item_title = (TextView) convertView.findViewById(R.id.order_item_title);
+            convertView.setTag(viewHolder);
+        }else{
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        viewHolder.order_item_title.setText(mData.get(position).getGoods_title());
+        return convertView;
+    }
+    private class ViewHolder{
+        TextView order_item_title;
+    }
+
+}
