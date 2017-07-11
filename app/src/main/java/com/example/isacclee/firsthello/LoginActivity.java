30bce9fc7bd15b0ac1de170efaaf9fd210b2b480
Isacclee <447473281@qq.com>
2017-07-11 10:26:16 +0800

    Login

--- app/src/main/java/com/example/isacclee/firsthello/LoginActivity.java
+++ app/src/main/java/com/example/isacclee/firsthello/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.isacclee.firsthello;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -39,21 +40,14 @@ import static android.Manifest.permission.READ_CONTACTS;
  */
 public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
 
-    /**
-     * Id to identity READ_CONTACTS permission request.
-     */
+
     private static final int REQUEST_READ_CONTACTS = 0;
 
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
+
     private static final String[] DUMMY_CREDENTIALS = new String[]{
             "foo@example.com:hello", "bar@example.com:world"
     };
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
+
     private UserLoginTask mAuthTask = null;
 
     // UI references.
@@ -61,6 +55,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private EditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
+
+    private String email;
+    private String password;
+    private String phone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -185,18 +183,18 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
             showProgress(true);
+            this.email=email;
+            this.password=password;
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
         }
     }
 
     private boolean isEmailValid(String email) {
-        //TODO: Replace this with your own logic
         return email.contains("@");
     }
 
     private boolean isPasswordValid(String password) {
-        //TODO: Replace this with your own logic
         return password.length() > 4;
     }
 
@@ -322,9 +320,13 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                     return pieces[1].equals(mPassword);
                 }
             }
-
-            // TODO: register the new account here.
             return true;
+//            Controller a = new Controller();
+//            int re = a.SignIn(mEmail,mPassword);
+//            if(re == 1)
+//                return true;
+//            else return false;
+            // TODO: register the new account here.
         }
 
         @Override
@@ -333,6 +335,19 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
+                Intent intent = new Intent();
+                intent.setClass(LoginActivity.this,MainActivity.class );
+
+                EditText thisEmail = (EditText)findViewById(R.id.email);
+                String ThisName = thisEmail.getText().toString();
+                Bundle bundle=new Bundle();
+                bundle.putString("email",LoginActivity.this.email);
+                bundle.putString("password",LoginActivity.this.password);
+                intent.putExtras(bundle);
+                System.out.println(bundle);
+                intent.putExtra("thisName",ThisName);
+
+                startActivity(intent);
                 finish();
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
