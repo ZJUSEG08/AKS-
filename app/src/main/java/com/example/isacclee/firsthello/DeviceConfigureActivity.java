12e1ffd96653858b40f61819ac15f99f1ed6528f
Isacclee <447473281@qq.com>
2017-07-14 14:05:14 +0800

    device configure

--- app/src/main/java/com/example/isacclee/firsthello/DeviceConfigureActivity.java
+++ app/src/main/java/com/example/isacclee/firsthello/DeviceConfigureActivity.java
@@ -16,23 +16,33 @@ public class DeviceConfigureActivity extends AppCompatActivity implements NfcAda
 
     private NfcAdapter mNfcAdapter;
     TextView textViewID;
+    TextView DeviceInfo;
+    TextView GoodsName;
+    TextView price;
+    TextView Address;
+    TextView deviceID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device_configure);
 
-        textViewID =(TextView) findViewById(R.id.textView4);
-//        TextView price = findViewById(R.id.textView5);
-//        TextView orderInfo = findViewById(R.id.OrderInfo);
-//        TextView expressState = findViewById(R.id.express_state);
-//        TextView expressID = findViewById(R.id.expressid);
+        GoodsName =(TextView) findViewById(R.id.textView4);
+        TextView price = (TextView)findViewById(R.id.textView5);
+        TextView DeviceID = (TextView)findViewById(R.id.textView);
+        TextView Address= (TextView)findViewById(R.id.textView7);
 //        getArgument获取传递过来的Bundle对象
-//        name.setText(getArguments().getString("name"));
-//        orderInfo.setText(getArguments().getString("OrderInfo"));
-//        price.setText(getArguments().getString("price"));
-//        expressID.setText(getArguments().getString("ExpressID"));
-//        expressState.setText(getArguments().getString("ExpressState"));
+
+        Controller controller = new Controller();
+        GoodsStructure goodsStructure = new GoodsStructure();
+        goodsStructure = controller.GoodsInfo(getApplicationContext(),deviceID.getText().toString());
+        GoodsName.setText(goodsStructure.getGoodsName());
+        price.setText(goodsStructure.getPrice().toString());
+
+        DeviceStructure deviceStructure = new DeviceStructure();
+        deviceStructure.aksID = deviceID.getText().toString();
+        controller.GetDevice(deviceStructure);
+        Address.setText("收货人："+deviceStructure.receiverName+"\n地址："+deviceStructure.address+"\n邮编:"+deviceStructure.postCode+"\n电话:"+deviceStructure.phone);
 
         // Check for available NFC Adapter
         mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
@@ -90,6 +100,8 @@ public class DeviceConfigureActivity extends AppCompatActivity implements NfcAda
             textViewID.append("\nnot null");
             textViewID.append(new String(msgID.getRecords()[0].getPayload()));
             textViewID.append("\n");
+            deviceID.setText(new String(msgID.getRecords()[0].getPayload()));
         }
+
     }
 }
